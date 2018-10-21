4b7620c317bfdb16b5afba0bbfaa7d60f80ae625
Levan <levan.list@gmail.com>
2018-10-21 22:07:56 +0200

    cretate methods: max 4E customer balance , return all money

--- Kaffemaschine/CoffeeMachine.cs
+++ Kaffemaschine/CoffeeMachine.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
-
+using System.Windows;
 
 namespace Kaffemaschine
 {
@@ -22,36 +22,56 @@ namespace Kaffemaschine
             Milch
         };
 
-       
-
 
         public CoffeeMachine() //constructor (erstellt die Klasse)
         {
 
         }
 
-        public enum DropedCoin
-        {
-            None,
-            zweiEuro,
-            EinsEuro,
-            funfzigCent,
-            
-
-
-
-
-        };
-
 
         /// <summary>
         /// Returns Costumer Balance + inserted Money
         /// </summary>
-        public double GetCustomerBalance()
+        public double  GetCustomerBalance()
         {
-
-            return CustomerBalance = _customerBalance + _moneyInserted;
+            double balance = _customerBalance + _moneyInserted;
+            if (balance <= 4)
+            {
+                return balance;
+            }
+            else
+            {
+                MessageBox.Show("You won't need more than 4€");
+                return _customerBalance;
+            }
         }
+
+
+        /// <summary>
+        /// returns all the money inserted
+        /// </summary>
+        public double ReturnMoney()
+        {
+            return CustomerBalance = 0;
+        }
+
+        /// <summary>
+        /// How much change will be returned and which coints 
+        /// like 
+        /// </summary>
+        /// <returns></returns>
+        public double GiveChange()
+        {
+            double moneyToBeReturned = _customerBalance - _priceToPay;
+
+            while(moneyToBeReturned != 0)
+            {
+
+            }
+
+
+        }
+
 
         private double _moneyInserted;
 
@@ -61,9 +81,8 @@ namespace Kaffemaschine
             return _moneyInserted;
         }
 
-        private Drink _selectedDrink;
-            
 
+        private Drink _selectedDrink;
         /// <summary>
         /// was gerade ausgewählt ist.
         /// </summary>
@@ -78,7 +97,6 @@ namespace Kaffemaschine
                 OnPropertyChanged("SelectedDrink");
             }
         }
-
 
 
         /// <summary>
@@ -105,14 +123,11 @@ namespace Kaffemaschine
         }
 
 
-
-
         private double _priceToPay;
         /// <summary>
         /// Was der Kunde zahlen muss
         /// </summary>
         public double PriceToPay {
-
             get => _priceToPay;
             set
             {
@@ -135,7 +150,6 @@ namespace Kaffemaschine
                 OnPropertyChanged("CustomerBalance");
             }
         }
-
 
 
         public event PropertyChangedEventHandler PropertyChanged;
